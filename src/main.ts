41fa1182deb9b8ca095727aaddeb19ceaf0944b4
aaroluo <aaro.luo@gmail.com>
2021-05-30 18:29:21 +0300

    refactor

--- src/main.ts
+++ src/main.ts
@@ -4,64 +4,59 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 // import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
 // import { AfterimagePass } from 'three/examples/jsm/postprocessing/AfterImagePass.js'
 import Stats from 'three/examples/jsm/libs/stats.module.js'
-import hsv2rgb from './util'
+import { hsv2rgb, lorenzAttractor } from './util'
 
-const renderer = new THREE.WebGLRenderer({ preserveDrawingBuffer: true })
+let scene: THREE.Scene
+let camera: THREE.PerspectiveCamera
+let controls: OrbitControls
+let renderer: THREE.WebGLRenderer
+let clock: THREE.Clock
+let stats: Stats
 
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+let points: THREE.Points
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-camera.position.set(0, 0, 100);
+function init() {
+  scene = new THREE.Scene();
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.target.set(0, 0, 25)
-
-const clock = new THREE.Clock()
-
-const scene = new THREE.Scene();
-
-const stats = Stats()
-document.body.appendChild(stats.dom)
-
-const scale = 1
-const numPoints = 2000
-
-
-let verts = []
-let colors = []
-for (let i = 0; i < numPoints; i++) {
-
-  verts.push(Math.random()* 40, Math.random()* 40, Math.random()* 40)
-  const rgb = hsv2rgb(100, 1, 1)
-  colors.push(rgb[0], rgb[1], rgb[2])
+  renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+  camera.position.set(0, 0, 100);
+  
+  controls = new OrbitControls(camera, renderer.domElement)
+  controls.target.set(0, 0, 25)
+  
+  clock = new THREE.Clock()
+  
+  stats = Stats()
+  
+  document.body.appendChild(renderer.domElement)
+  document.body.appendChild(stats.dom)
 }
 
-const geometry = new THREE.BufferGeometry()
-geometry.setAttribute('position', new THREE.Float32BufferAttribute(verts, 3))
-geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
-// // uniforms
-// const uniforms = {
-//   color: { value: new THREE.Color( 0xffff00 ) },
-// };
+function initPoints() {
+  const scale = .6
+  const numPoints = 5000
 
-// // point cloud material
-// const shaderMaterial = new THREE.ShaderMaterial( {
-//   uniforms: uniforms,
-//   vertexShader: document.getElementById( 'vertexshader' )?.textContent!,
-//   fragmentShader: document.getElementById( 'fragmentshader' )?.textContent!,
-//   transparent:  true
-// });
-const material = new THREE.PointsMaterial({ size: scale, vertexColors: true })
-const points = new THREE.Points(geometry, material)
-scene.add(points)
+  let verts = []
+  let colors = []
+  for (let i = 0; i < numPoints; i++) {
 
-// const gridHelper = new THREE.GridHelper(50, 10);
-// scene.add(gridHelper);
+    verts.push(Math.random() * 40, Math.random() * 40, Math.random() * 40)
+    const rgb = hsv2rgb(1, 0, 1)
+    colors.push(rgb[0], rgb[1], rgb[2])
+  }
 
-const rho = 28.0
-const sigma = 10.0
-const beta = 8.0 / 3.0
+  const geometry = new THREE.BufferGeometry()
+  geometry.setAttribute('position', new THREE.Float32BufferAttribute(verts, 3))
+  geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
+
+  const material = new THREE.PointsMaterial({ size: scale, vertexColors: true })
+  points = new THREE.Points(geometry, material)
+  scene.add(points)
+}
+
 
 function animate() {
   requestAnimationFrame(animate)
@@ -73,7 +68,7 @@ function animate() {
     const x = verts.getX(i)
     const y = verts.getY(i)
     const z = verts.getZ(i)
-    const o = f([x, y, z], s / 5)
+    const o = lorenzAttractor([x, y, z], s / 5)
 
     verts.setXYZ(i, x + o[0], y + o[1], z + o[2])
   }
@@ -85,17 +80,9 @@ function animate() {
   renderer.render(scene, camera)
 }
 
-stats.begin()
+init()
+initPoints()
 animate()
-
-function f(current: [number, number, number], t: number) {
-  let [x, y, z] = current;
-
-  let dx = sigma * (y - x)
-  let dy = x * (rho - z) - y
-  let dz = x * y - beta * z
-  return [dx * t, dy * t, dz * t]
-}
 
 window.addEventListener('resize', onWindowResize, false);
 
--- src/util.ts
+++ src/util.ts
@@ -5,4 +5,20 @@ function hsv2rgb(h: number,s: number,v: number)
   return [f(5),f(3),f(1)];       
 }
 
-export default hsv2rgb
+const rho = 28.0
+const sigma = 10.0
+const beta = 8.0 / 3.0
+
+function lorenzAttractor(current: [number, number, number], t: number) {
+  let [x, y, z] = current;
+
+  let dx = sigma * (y - x)
+  let dy = x * (rho - z) - y
+  let dz = x * y - beta * z
+  return [dx * t, dy * t, dz * t]
+}
+
+export {
+  hsv2rgb,
+  lorenzAttractor,
+}
